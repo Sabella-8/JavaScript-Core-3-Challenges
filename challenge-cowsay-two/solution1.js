c7002c6a3c7cf59a3bbb00f7b54f361a1fc6d415
Sabella-8 <gsabella549@gmail.com>
2024-02-14 16:57:19 +0000

    saying

--- challenge-cowsay-two/solution1.js
+++ challenge-cowsay-two/solution1.js
@@ -1,5 +1,5 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries
 // https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 // =================
@@ -10,19 +10,16 @@
 
 // 2. Make supplies for our speech bubble
 
-let topLine = '_';
-let bottomLine = '-';
-let saying = '';
+let topLine = "_";
+let bottomLine = "-";
+let saying = "hello";
 
 // 3. Make a cow that takes a string
 
 function cowsay(saying) {
-// how will you make the speech bubble contain the text?
-
-// where will the cow picture go?
-
-// how will you account for the parameter being empty?
-
+  // how will you make the speech bubble contain the text?
+  // where will the cow picture go?
+  // how will you account for the parameter being empty?
 }
 
 //4. Pipe argument into cowsay function and return a cow
